8d08dc3789e35fef2fd941ab67fdaf66a7234193
Hervé Ménager <herve.menager@gmail.com>
2016-02-04 17:03:34 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#from distutils.core import setup
+import glob
+from setuptools import setup
+
+readme = open('README.md').read()
+
+setup(
+    name='edamxpathvalidator',
+    version='0.1',
+    description='validation of EDAM internal consistency',
+    long_description=readme,
+    author='Hervé Ménager',
+    author_email='hmenager@pasteur.fr',
+    url='https://github.com/edamontology/ReMoTE.git',
+    packages=['remote'],
+    install_requires=[
+          'lxml'
+    ],
+    license="BSD",
+    entry_points={
+          'console_scripts': ['remote=remote:main'],
+        },
+    include_package_data=True,
+    zip_safe=False 
+)
