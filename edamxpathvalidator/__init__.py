313777fd9da6de648a5527c85bfe06b9829d4573
Hervé Ménager <hmenager@pasteur.fr>
2016-02-16 14:50:28 +0100

    add a test to detect term superclass of itself.
    bug is described here:
    https://github.com/edamontology/edamontology/issues/114

--- edamxpathvalidator/__init__.py
+++ edamxpathvalidator/__init__.py
@@ -31,33 +31,40 @@ def check_file(file_path):
     """ check the consistency of the EDAM file specified by a filesystem path """
     doc = etree.parse(file_path)
     #listing obsolete terms pointing to other obsolete terms as consider or replacement...
-    els = doc.xpath("//owl:Class[owl:deprecated='true']", namespaces=EDAM_NS)
+    els = doc.xpath("//owl:Class", namespaces=EDAM_NS)
     for element in els:
-        consider_ids = element.xpath('oboInOwl:consider/@rdf:resource', namespaces=EDAM_NS)
-        if consider_ids:
-            consider_id = consider_ids[0]
-            targets = doc.xpath("//owl:Class[@rdf:about='" +\
-                                consider_id+"' and owl:deprecated='true']", \
-                                namespaces=EDAM_NS)
-            if targets:
-                report(element, targets, "obsolete consider obsolete")
-        replaced_by_ids = element.xpath('oboInOwl:replacedBy/@rdf:resource', namespaces=EDAM_NS)
-        if replaced_by_ids:
-            replaced_by_id = replaced_by_ids[0]
-            targets = doc.xpath("//owl:Class[@rdf:about='" +\
-                                replaced_by_id + "' and owl:deprecated='true']",\
-                                namespaces=EDAM_NS)
-            if targets:
-                report(element, targets, "obsolete replacedBy obsolete")
-                source_id = element.xpath('@rdf:about', namespaces=EDAM_NS)[0]
-                source_axis = source_id.split('/').pop().split('_')[0]
-                target_axis = replaced_by_id.split('/').pop().split('_')[0]
-                if source_axis != target_axis:
-                    targets = doc.xpath("//owl:Class[@rdf:about='" +\
-                                        replaced_by_id + "']",\
-                                        namespaces=EDAM_NS)
-                    report(element, targets, "obsolete '" + source_axis +\
-                           "' term replacedBy '" + target_axis + "' term")
+        for superclass_id in element.xpath('rdfs:subClassOf/@rdf:resource', namespaces=EDAM_NS):
+            source_id = element.xpath('@rdf:about', namespaces=EDAM_NS)[0]
+            if superclass_id==source_id:
+                targets = doc.xpath("//owl:Class[@rdf:about='" + superclass_id+"']", \
+                                     namespaces=EDAM_NS)
+                report(element, targets, "Element " + source_id + " is superclass of itself")
+        if element.xpath("owl:deprecated='true'", namespaces=EDAM_NS):
+            consider_ids = element.xpath('oboInOwl:consider/@rdf:resource', namespaces=EDAM_NS)
+            if consider_ids:
+                consider_id = consider_ids[0]
+                targets = doc.xpath("//owl:Class[@rdf:about='" +\
+                                    consider_id+"' and owl:deprecated='true']", \
+                                    namespaces=EDAM_NS)
+                if targets:
+                    report(element, targets, "obsolete consider obsolete")
+            replaced_by_ids = element.xpath('oboInOwl:replacedBy/@rdf:resource', namespaces=EDAM_NS)
+            if replaced_by_ids:
+                replaced_by_id = replaced_by_ids[0]
+                targets = doc.xpath("//owl:Class[@rdf:about='" +\
+                                    replaced_by_id + "' and owl:deprecated='true']",\
+                                    namespaces=EDAM_NS)
+                if targets:
+                    report(element, targets, "obsolete replacedBy obsolete")
+                    source_id = element.xpath('@rdf:about', namespaces=EDAM_NS)[0]
+                    source_axis = source_id.split('/').pop().split('_')[0]
+                    target_axis = replaced_by_id.split('/').pop().split('_')[0]
+                    if source_axis != target_axis:
+                        targets = doc.xpath("//owl:Class[@rdf:about='" +\
+                                            replaced_by_id + "']",\
+                                            namespaces=EDAM_NS)
+                        report(element, targets, "obsolete '" + source_axis +\
+                               "' term replacedBy '" + target_axis + "' term")
 
 def main():
     """ function called by the module script """
